2348da8aef77c24b704a93895dd5636984adc36e
warren.golubski <warren.golubski@gmail.com>
2020-06-07 14:52:18 -0700

    added unit tests

---
+++ test_GettingStarted.py
@@ -0,0 +1,23 @@
+#Testin with pytest
+import baseClass as bc
+
+
+class TestClass:
+    def test_warren(self):
+        me = bc.baseClass()
+        assert 0 == me.beer
+
+    def test_givebeer(self):
+        me = bc.baseClass()
+        me.giveBeer()
+        assert 1 == me.beer
+
+    def test_initial(self):
+        me = bc.baseClass()
+        assert False == me.isHappy()
+
+    def test_method(self):
+        me = bc.baseClass()
+        me.giveBeer()
+        assert True == me.isHappy()
+
